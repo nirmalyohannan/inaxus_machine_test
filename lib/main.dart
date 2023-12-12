697a8cf394d0aadc3eaa962a3fe184d49a6ed554
Nirmal Yohannan <n.yohannan@avua.com>
2023-12-12 14:07:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:inaxus_machine_test/ui/screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inaxus Machine Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
